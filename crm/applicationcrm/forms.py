230bd38765034f18f641980884e3e89a24821bf0
Vladislav Dubov <vladdubov699@gmail.com>
2022-10-04 16:12:49 +0300

    Added uploading file logic

--- crm/applicationcrm/forms.py
+++ crm/applicationcrm/forms.py
@@ -3,4 +3,3 @@ from django import forms
 
 class UserForm(forms.Form):
     name = forms.FileField()
-    age = forms.IntegerField()
--- crm/applicationcrm/views.py
+++ crm/applicationcrm/views.py
@@ -1,8 +1,26 @@
+from django.core.files.storage import FileSystemStorage
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from .forms import UserForm
+
 
 # Create your views here.
 
 def index(request):
-    userform = UserForm()
-    return render(request, 'homepage/index.html', {'form': userform})
+    if request.method == "POST" and request.FILES["file"]:
+        form = UserForm
+        myfile = request.FILES["file"]
+        fs = FileSystemStorage()
+        filename = fs.save(myfile.name, myfile)
+        uploaded_file_url = fs.url(filename)
+        return render(request, 'homepage/index.html', {
+            'form': form
+        })
+
+    return render(request, 'homepage/index.html')
+
+
+def handle_uploaded_file(f):
+    with open('/aaaaaaaaaaaaaaaa/', 'wb+') as destination:
+        for chunk in f.chunks():
+            destination.write(chunk)
